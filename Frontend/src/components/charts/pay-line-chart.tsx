b5fcd88e9de570ae9bf78547a38fd8437d65ff6d
valceven <valmykelcevenbolante@gmail.com>
2025-06-08 05:53:06 +0800

    Fixed es-lint errors

--- Frontend/src/components/charts/pay-line-chart.tsx
+++ Frontend/src/components/charts/pay-line-chart.tsx
@@ -1,16 +1,6 @@
 "use client"
 
-import { TrendingUp } from "lucide-react"
 import { CartesianGrid, Line, LineChart, XAxis } from "recharts"
-
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
 import {
   ChartConfig,
   ChartContainer,
