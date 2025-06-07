fda258a933728d84c6d99bfcee73ce81987f3982
valceven <valmykelcevenbolante@gmail.com>
2025-06-08 06:01:23 +0800

    commented temporary files

--- Frontend/src/app/auth/login/dashboard/layout.tsx
+++ Frontend/src/app/auth/login/dashboard/layout.tsx
@@ -1,22 +1,22 @@
 "use client";
 
-import { useAuth } from "@/context/AuthContext";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+//import { useAuth } from "@/context/AuthContext";
+//import { useRouter } from "next/navigation";
+//import { useEffect } from "react";
 
 export default function DashboardLayout({ children }: { children: React.ReactNode}) {
-    const { user, isLoading } = useAuth();
-    const router = useRouter();
+    //const { user, isLoading } = useAuth();
+    //const router = useRouter();
 
-    useEffect(() => {
-        if (!isLoading) {
-            router.push("auth/login");
-        }
-    }, [user, isLoading, router]);
+    // useEffect(() => {
+    //     if (!isLoading) {
+    //         router.push("auth/login");
+    //     }
+    // }, [user, isLoading, router]);
 
-    if (isLoading) {
-        return <div className="flex items-center justify-center h-screen">Loading...</div>
-    }
+    // if (isLoading) {
+    //     return <div className="flex items-center justify-center h-screen">Loading...</div>
+    // }
 
     // if (!user) {
     //     return null;
